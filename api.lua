18d231f66a465f10f5d33ce960ec74d306271e44
david <beanzilla@red-green.com>
2021-06-10 14:00:06 -0400

    Added MineClone2 Support
      Note: This may not work... and may fail, please report it as an issue
    if yours doesn't work. (I will try my best to get it to work)

--- api.lua
+++ api.lua
@@ -1,59 +1,136 @@
 -- Attempt to just get the internals to enable calls to work
 local default = rawget(_G, "default") or nil
-local hopper = rawget(_G, "hopper") or nil
 -- support for MT game translation.
-local S = default.get_translator
+local S = minetest.get_translator("item_generator")
 
 function igen.add_generator(itemstring, perprocess, processdelay)
     -- Here is where we would add a generator to crafting
     local infotext_name = igen_internal.split(itemstring, ':')[2]
-    igen_internal.register_chest("item_generator:generator_"..infotext_name, {
-        description = S("Generator "..igen_internal.firstToUpper(infotext_name)),
-        tiles = {
-            "default_chest_top.png",
-            "default_chest_top.png",
-            "default_chest_side.png",
-            "default_chest_side.png",
-            "default_chest_lock.png",
-            "default_chest_inside.png"
-        },
-        sounds = default.node_sound_wood_defaults(),
-        sound_open = "default_chest_open",
-        sound_close = "default_chest_close",
-        groups = {choppy = 2, oddly_breakable_by_hand = 2},
-        protected = true,
-        product = itemstring,
-        product_amt = perprocess,
-        product_rate = processdelay,
-    })
+    if igen_secure_generators and not igen_mcl2_support then
+        igen_internal.register_chest("item_generator:generator_"..infotext_name, {
+            description = S("Generator "..igen_internal.firstToUpper(infotext_name)),
+            tiles = {
+                "igen_chest_top.png",
+                "igen_chest_top.png",
+                "igen_chest_side.png",
+                "igen_chest_side.png",
+                "igen_chest_lock.png",
+                "igen_chest_inside.png"
+            },
+            sounds = igen_internal.node_sound_wood_defaults(),
+            sound_open = "igen_chest_open",
+            sound_close = "igen_chest_close",
+            groups = {choppy = 2, oddly_breakable_by_hand = 2},
+            protected = true,
+            product = itemstring,
+            product_amt = perprocess,
+            product_rate = processdelay,
+        })
+    else
+        igen_internal.register_unsecure_chest("item_generator:generator_"..infotext_name, {
+            description = S("Generator "..igen_internal.firstToUpper(infotext_name)),
+            tiles = {
+                "igen_chest_top.png",
+                "igen_chest_top.png",
+                "igen_chest_side.png",
+                "igen_chest_side.png",
+                "igen_chest_lock.png",
+                "igen_chest_inside.png"
+            },
+            sounds = igen_internal.node_sound_wood_defaults(),
+            sound_open = "igen_chest_open",
+            sound_close = "igen_chest_close",
+            groups = {choppy = 2, oddly_breakable_by_hand = 2},
+            protected = true,
+            product = itemstring,
+            product_amt = perprocess,
+            product_rate = processdelay,
+        })
+    end
 
     -- Make a craft simular to the locked chest
-    minetest.register_craft({
-        output = "item_generator:generator_"..infotext_name,
-        recipe = {
-            {"group:wood", itemstring, "group:wood"},
-            {"group:wood", "default:steel_ingot", "group:wood"},
-            {"group:wood", "group:wood", "group:wood"},
-        }
-    })
+    if default and igen_secure_generators then
+        minetest.register_craft({
+            output = "item_generator:generator_"..infotext_name,
+            recipe = {
+                {"group:wood", itemstring, "group:wood"},
+                {"group:wood", "default:steel_ingot", "group:wood"},
+                {"group:wood", "group:wood", "group:wood"},
+            }
+        })
+    elseif default and not igen_secure_generators then
+        minetest.register_craft({
+            output = "item_generator:generator_"..infotext_name,
+            recipe = {
+                {"group:wood", itemstring, "group:wood"},
+                {"group:wood", "", "group:wood"},
+                {"group:wood", "group:wood", "group:wood"},
+            }
+        })
+    elseif igen_mcl2_support then
+        minetest.register_craft({
+            output = "item_generator:generator_"..infotext_name,
+            recipe = {
+                {"group:wood", itemstring, "group:wood"},
+                {"group:wood", "", "group:wood"},
+                {"group:wood", "group:wood", "group:wood"},
+            }
+        })
+    end
 
     -- Also include conversion methods for locked chests to item generators
-    minetest.register_craft({
-        type = "shapeless",
-        output = "item_generator:generator_"..infotext_name,
-        recipe = {
-            "default:chest_locked",
-            itemstring,
-        }
-    })
+    if default and igen_secure_generators then
+        minetest.register_craft({
+            type = "shapeless",
+            output = "item_generator:generator_"..infotext_name,
+            recipe = {
+                "default:chest_locked",
+                itemstring,
+            }
+        })
 
-    -- Downgrade Option, Incase you want a locked chest back
-    minetest.register_craft({
-        type = "cooking",
-        output = "default:chest_locked",
-        recipe = "item_generator:generator_"..infotext_name,
-        cooktime = 5,
-    })
+        -- Downgrade Option, Incase you want a locked chest back
+        minetest.register_craft({
+            type = "cooking",
+            output = "default:chest_locked",
+            recipe = "item_generator:generator_"..infotext_name,
+            cooktime = 5,
+        })
+    elseif default and not igen_secure_generators then
+        minetest.register_craft({
+            type = "shapeless",
+            output = "item_generator:generator_"..infotext_name,
+            recipe = {
+                "default:chest",
+                itemstring,
+            }
+        })
+
+        -- Downgrade Option, Incase you want a locked chest back
+        minetest.register_craft({
+            type = "cooking",
+            output = "default:chest",
+            recipe = "item_generator:generator_"..infotext_name,
+            cooktime = 5,
+        })
+    elseif igen_mcl2_support then
+        minetest.register_craft({
+            type = "shapeless",
+            output = "item_generator:generator_"..infotext_name,
+            recipe = {
+                "mcl_chests:chest",
+                itemstring,
+            }
+        })
+
+        -- Downgrade Option, Incase you want a locked chest back
+        minetest.register_craft({
+            type = "cooking",
+            output = "mcl_chests:chest",
+            recipe = "item_generator:generator_"..infotext_name,
+            cooktime = 5,
+        })
+    end
 
     -- Fuel Option, Incase you really want to dispose of the generator
     minetest.register_craft({
@@ -61,27 +138,6 @@ function igen.add_generator(itemstring, perprocess, processdelay)
         recipe = "item_generator:generator_"..infotext_name,
         burntime = 35, -- About 5 more seconds compared to default:chest and default:chest_locked
     })
-    if igen_log_generator_creation then
-        minetest.log("action", "[item_generator] Added Generator for '"..itemstring.."' which will produce "..perprocess.." at a rate of "..processdelay.." seconds.")
-    end
 
-    if hopper and igen_hopper_support then -- Add hopper support for some things
-        hopper:add_container({
-            -- Allow hopper to extract resources (Not allowed since this could be a method of exploit)
-		    --{"top", "item_generator:generator_"..infotext_name, "main"},
-            -- Allow hopper to insert resources
-		    {"bottom", "item_generator:generator_"..infotext_name, "main"},
-            -- Allow hopper to insert resources
-		    {"side", "item_generator:generator_"..infotext_name, "main"},
-            -- Allow hopper to extract resources (Potential exploit)
-		    --{"top", "item_generator:generator_"..infotext_name.."_open", "main"},
-            -- Allow hopper to insert resources
-		    {"bottom", "item_generator:generator_"..infotext_name.."_open", "main"},
-            -- Allow hopper to insert resources
-		    {"side", "item_generator:generator_"..infotext_name.."_open", "main"},
-        })
-    end
-    if igen_log_hopper_support and igen_log_generator_creation then
-        minetest.log("action", "[item_generator] Added hopper support for '"..itemstring.."' Generator.")
-    end
+    minetest.log("action", "[item_generator] Added Generator for '"..itemstring.."' which will produce "..perprocess.." at a rate of "..processdelay.." seconds.")
 end
